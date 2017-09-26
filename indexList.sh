2c19cf30b7b83c3d3f683a783558ba4808e5f4b0
Raj Raulji <raj.raulji@blackbaud.com>
2017-09-26 12:12:27 -0500

    LUM-17722 - removed echo statement.

--- indexList.sh
+++ indexList.sh
@@ -4,6 +4,4 @@ source "./indexUtil.sh"
 buildElasticSearchCatUrl elasticSearchCatUrl $environment
 echo $elasticSearchCatUrl
 
-echo $username
-echo $password
 curl -k --user $username -X GET $elasticSearchCatUrl/indices?v
